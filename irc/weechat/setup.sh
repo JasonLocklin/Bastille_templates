3c7e49a6a98d39cbc75fcb221a3ec7191790dd50
Jason Locklin <jason@locklin.science>
2026-06-03 12:08:03 -0500

    Bastille commands

--- irc/weechat/setup.sh
+++ irc/weechat/setup.sh
@@ -7,3 +7,9 @@ zfs create zroot/jails/weechat/local
 
 
 chown -R 1001:1001 /zroot/jails/weechat
+
+
+bastille create weechat 15.0-RELEASE 10.10.10.3 em0
+
+bastille template weechat /usr/local/bastille/templates/Bastille_templates/irc/weechat
+
